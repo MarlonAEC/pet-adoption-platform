f83e93ff33c3bff9e1f1d8b25a2ef3af3b08d833
Marlon Alejandro Espinosa Castañeiras <marlon9402@outlook.com>
2024-10-19 21:57:24 -0400

    added button component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TypographyComponent],
+  imports: [RouterOutlet, TypographyComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pet-adoption-frontend';
+  isButtonDisabled: boolean = true;
+
+  // Function to handle the click event
+  handleClick() {
+    console.log('Button clicked');
+  }
 }
---
+++ frontend/src/app/button/button.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TypographyComponent } from '../typography/typography.component';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule, TypographyComponent],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() label: string = 'Click Me'; // Button label
+  @Input() customClass: string = '';   // Optional class for custom styling
+  @Input() disabled: boolean = false;  // Disabled state
+
+  @Output() onClick: EventEmitter<void> = new EventEmitter<void>(); // Event emitter for click
+
+  handleClick() {
+    this.onClick.emit(); // Emit the click event
+  }
+}
