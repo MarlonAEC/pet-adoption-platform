03593d4a175e517447205e44336ef6c8fa7077ee
Marlon Alejandro Espinosa Castañeiras <marlon9402@outlook.com>
2024-12-03 21:29:17 -0500

    create styles for logout page

--- frontend/src/app/pages/logout-page/logout-page.component.ts
+++ frontend/src/app/pages/logout-page/logout-page.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { TypographyComponent } from "../../components/typography/typography.component";
 
 @Component({
   selector: 'app-logout-page',
   standalone: true,
-  imports: [],
+  imports: [TypographyComponent],
   templateUrl: './logout-page.component.html',
   styleUrl: './logout-page.component.css'
 })
 export class LogoutPageComponent {
+  countdown: number = 5;
+
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router
@@ -19,9 +22,20 @@ export class LogoutPageComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.authService.logout();
-    setTimeout(() => {
-      this.router.navigate(['/login']);
-    }
-    , 3000);
+    this.startCountdown();
+    // setTimeout(() => {
+    //   this.router.navigate(['/login']);
+    // }
+    // , 3000);
+  }
+
+  startCountdown(): void {
+    const interval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown === 0) {
+        clearInterval(interval);
+        this.router.navigate(['/login']);
+      }
+    }, 1000);
   }
 }
